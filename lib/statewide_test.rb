15b0380a7a7e891e4342314575dc73f2a12b7b3b
ccorrigan <barnumswinter@gmail.com>
2017-04-17 16:46:29 -0600

    loads all files and passes all file data to statewide_test.rb

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -1,9 +1,12 @@
 class StatewideTest
-  attr_reader :name, :third_grade_data
+  attr_reader :name, :third_grade_data, :eighth_grade_data, :math_data, :reading_data, :writing_data
 
   def initialize(args)
     @name = args[:name]
     @third_grade_data = args[:third_grade_data]
-    # @graduation_rate = args[:graduation_rate]
+    @eighth_grade_data = args[:eighth_grade_data]
+    @math_data = args[:math_data]
+    @reading_data = args[:reading_data]
+    @writing_data = args[:writing_data]
   end
 end
