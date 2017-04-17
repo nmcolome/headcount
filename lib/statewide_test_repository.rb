74f2f4969af9dfa294328708d332f3e9d920ab2c
ccorrigan <barnumswinter@gmail.com>
2017-04-17 17:47:09 -0600

    refactor initialize_instances helper methods

--- lib/statewide_test_repository.rb
+++ lib/statewide_test_repository.rb
@@ -17,11 +17,11 @@ class StatewideTestRepository
   def initialize_instances(data_set)
     @statewide_tests = {}
     unique_districts(data_set).each do |district_name|
-      third_grade_data = get_third_grade_data(district_name, data_set)
-      eighth_grade_data = get_eighth_grade_data(district_name, data_set)
-      math_data = get_math_data(district_name, data_set)
-      reading_data = get_reading_data(district_name, data_set)
-      writing_data = get_writing_data(district_name, data_set)
+      third_grade_data = get_testing_data_by_grade(district_name, data_set, :third_grade)
+      eighth_grade_data = get_testing_data_by_grade(district_name, data_set, :eighth_grade)
+      math_data = get_testing_data_by_ethnicity(district_name, data_set, :math)
+      reading_data = get_testing_data_by_ethnicity(district_name, data_set, :reading)
+      writing_data = get_testing_data_by_ethnicity(district_name, data_set, :writing)
       @statewide_tests[district_name.upcase] = StatewideTest.new({
                                                     :name => district_name.upcase,
                                                     :third_grade_data => third_grade_data,
@@ -37,55 +37,10 @@ class StatewideTestRepository
     statewide_tests[district_name.upcase]
   end
 
-  def get_third_grade_data(district_name, data_set)
-    third_grade_data = {}
-    district_data = get_district_third_data(district_name, data_set)
-    unique_years = find_unique_years(district_data) #[2008, 2009, 2010]
-    unique_years.each do |year|
-      third_grade_data[year.to_i] = {}
-      district_data.each do |row|
-        if row[:timeframe] == year
-          third_grade_data[year.to_i][(row[:score]).downcase.to_sym] = row[:data]
-        end
-      end
-    end
-    third_grade_data
-  end
-
-  def get_eighth_grade_data(district_name, data_set)
-    eighth_grade_data = {}
-    district_data = get_district_eighth_data(district_name, data_set)
-    unique_years = find_unique_years(district_data) #[2008, 2009, 2010]
-    unique_years.each do |year|
-      eighth_grade_data[year.to_i] = {}
-      district_data.each do |row|
-        if row[:timeframe] == year
-          eighth_grade_data[year.to_i][(row[:score]).downcase.to_sym] = row[:data]
-        end
-      end
-    end
-    eighth_grade_data
-  end
-
-    def get_math_data(district_name, data_set)
-    math_data = {}
-    district_data = get_district_math_data(district_name, data_set)
-    unique_years = find_unique_years(district_data) #[2008, 2009, 2010]
-    unique_years.each do |year|
-      math_data[year.to_i] = {}
-      district_data.each do |row|
-        if row[:timeframe] == year
-          math_data[year.to_i][(row[:race_ethnicity]).downcase.to_sym] = row[:data]
-        end
-      end
-    end
-    math_data
-  end
-
-    def get_district_math_data(district_name, data_set)
+  def get_district_data(district_name, data_set, file_key)
     district_data = []
-    data_set[:statewide_testing][:math].rewind
-    data_set[:statewide_testing][:math].each do |row|
+    data_set[:statewide_testing][file_key].rewind
+    data_set[:statewide_testing][file_key].each do |row|
       if district_name.upcase == row[:location].upcase
         district_data << row
       end
@@ -93,86 +48,40 @@ class StatewideTestRepository
     district_data
   end
 
-
-  def get_district_third_data(district_name, data_set)
-    district_data = []
-    data_set[:statewide_testing][:third_grade].rewind
-    data_set[:statewide_testing][:third_grade].each do |row|
-      if district_name.upcase == row[:location].upcase
-        district_data << row
-      end
-    end
-    district_data
-  end
-
-    def get_district_eighth_data(district_name, data_set)
-    district_data = []
-    data_set[:statewide_testing][:eighth_grade].rewind
-    data_set[:statewide_testing][:eighth_grade].each do |row|
-      if district_name.upcase == row[:location].upcase
-        district_data << row
-      end
-    end
-    district_data
-  end
-
-def find_unique_years(district_data)
-  unique_years = district_data.map do |row|
-    row[:timeframe]
-  end
-  unique_years.uniq!
-end
-
-    def get_reading_data(district_name, data_set)
-    reading_data = {}
-    district_data = get_district_reading_data(district_name, data_set)
-    unique_years = find_unique_years(district_data) #[2008, 2009, 2010]
+  def get_testing_data_by_grade(district_name, data_set, file_key)
+    district_data = get_district_data(district_name, data_set, file_key)
+    file_key = {}
+    unique_years = find_unique_years(district_data)
     unique_years.each do |year|
-      reading_data[year.to_i] = {}
+      file_key[year.to_i] = {}
       district_data.each do |row|
         if row[:timeframe] == year
-          reading_data[year.to_i][(row[:race_ethnicity]).downcase.to_sym] = row[:data]
+          file_key[year.to_i][(row[:score]).downcase.to_sym] = row[:data]
         end
       end
     end
-    reading_data
+    file_key
   end
 
-    def get_district_reading_data(district_name, data_set)
-    district_data = []
-    data_set[:statewide_testing][:reading].rewind
-    data_set[:statewide_testing][:reading].each do |row|
-      if district_name.upcase == row[:location].upcase
-        district_data << row
-      end
-    end
-    district_data
-  end
-
-    def get_writing_data(district_name, data_set)
-    writing_data = {}
-    district_data = get_district_writing_data(district_name, data_set)
-    unique_years = find_unique_years(district_data) #[2008, 2009, 2010]
+  def get_testing_data_by_ethnicity(district_name, data_set, file_key)
+    district_data = get_district_data(district_name, data_set, file_key)
+    file_key = {}
+    unique_years = find_unique_years(district_data)
     unique_years.each do |year|
-      writing_data[year.to_i] = {}
+      file_key[year.to_i] = {}
       district_data.each do |row|
         if row[:timeframe] == year
-          writing_data[year.to_i][(row[:race_ethnicity]).downcase.to_sym] = row[:data]
+          file_key[year.to_i][(row[:race_ethnicity]).downcase.to_sym] = row[:data]
         end
       end
     end
-    writing_data
+    file_key
   end
 
-    def get_district_writing_data(district_name, data_set)
-    district_data = []
-    data_set[:statewide_testing][:writing].rewind
-    data_set[:statewide_testing][:writing].each do |row|
-      if district_name.upcase == row[:location].upcase
-        district_data << row
-      end
+  def find_unique_years(district_data)
+    unique_years = district_data.map do |row|
+      row[:timeframe]
     end
-    district_data
+    unique_years.uniq!
   end
-
 end
--- test/statewide_test_repository_test.rb
+++ test/statewide_test_repository_test.rb
@@ -4,47 +4,47 @@ require './lib/statewide_test_repository.rb'
 
 class TestStatewideTestRepository < Minitest::Test
 
-  # def test_it_exists
-  #   str = StatewideTestRepository.new
+  def test_it_exists
+    str = StatewideTestRepository.new
     
-  #   assert_instance_of StatewideTestRepository, str
-  # end
+    assert_instance_of StatewideTestRepository, str
+  end
 
-  # def test_it_loads_data
-  #   str = StatewideTestRepository.new
-  #   data = str.load_data({
-  #       :statewide_testing => {
-  #       :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
-  #       :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
-  #       :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
-  #       :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
-  #       :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
-  #     }
-  #   })
-  #     assert_equal Hash, data.class
-  # end
+  def test_it_loads_data
+    str = StatewideTestRepository.new
+    data = str.load_data({
+        :statewide_testing => {
+        :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+        :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+        :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+      }
+    })
+      assert_equal Hash, data.class
+  end
 
-  # def test_find_by_name
-  #     str = StatewideTestRepository.new
-  #     str.load_data({
-  #       :statewide_testing => {
-  #       :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
-  #       :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
-  #       :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
-  #       :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
-  #       :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
-  #     }
-  #   })
+  def test_find_by_name
+      str = StatewideTestRepository.new
+      str.load_data({
+        :statewide_testing => {
+        :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+        :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+        :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+      }
+    })
 
-  #   output_1 = str.find_by_name("ACADEMY 20")
-  #   assert_equal StatewideTest, output_1.class
+    output_1 = str.find_by_name("ACADEMY 20")
+    assert_equal StatewideTest, output_1.class
 
-  #   output_2 = str.find_by_name("UNLIKELY NAME")
-  #   assert_nil output_2
+    output_2 = str.find_by_name("UNLIKELY NAME")
+    assert_nil output_2
 
-  #   output_3 = str.find_by_name("academy 20")
-  #   assert_equal StatewideTest, output_3.class
-  # end
+    output_3 = str.find_by_name("academy 20")
+    assert_equal StatewideTest, output_3.class
+  end
 
   def test_access_to_statewide_test_object_3rd_grade_data
     str = StatewideTestRepository.new
