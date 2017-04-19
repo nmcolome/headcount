2d5263aabedcb3d306297fa623123cb6ef793cbf
ccorrigan <barnumswinter@gmail.com>
2017-04-18 20:28:19 -0600

    Add #get_median_household_income

--- lib/economic_profile_repository.rb
+++ lib/economic_profile_repository.rb
@@ -1,30 +1,48 @@
 require 'csv'
 require_relative 'repository_module'
+require_relative 'economic_profile'
 
 class EconomicProfileRepository
   include Repository
+  attr_reader :economic_profiles
 
   def load_data(args)
     enrollment = args[:enrollment]
     statewide_testing = args[:statewide_testing]
     economic_profile = args[:economic_profile]
     data_set = get_data(args)
-    # initialize_instances(data_set)
-    # data_set
+    initialize_instances(data_set)
+    data_set
   end
 
   def initialize_instances(data_set)
     @economic_profiles = {}
     unique_districts(data_set).each do |district_name|
-      @economic_profiles[district_name.upcase] = Enrollment.new({
-                                                    :name => district_name.upcase,
-                                                    :kindergarten_participation => district_participation,
-                                                    :graduation_rate => district_graduation_rate
-                                                  })
+      median_household_income = get_median_household_income(data_set, district_name)
+      @economic_profiles[district_name.upcase] = EconomicProfile.new({
+                                                                :median_household_income => median_household_income,
+                                                                :children_in_poverty => {2012 => 0.1845},
+                                                                :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
+                                                                :title_i => {2015 => 0.543},
+                                                                :name => "ACADEMY 20"
+                                                              })
     end
   end
 
   def find_by_name(district_name)
     economic_profiles[district_name.upcase]
+  end
+
+  def get_median_household_income(data_set, district_name)
+    data_set[:economic_profile][:median_household_income].rewind
+    median_household_income = {}
+    data_set[:economic_profile][:median_household_income].each do |row|
+      if (row[:location]).upcase == district_name.upcase
+        key_name = row[:timeframe].split("-")
+        key_name.map! { |year| year.to_i }
+        median_household_income[key_name] = row[:data].to_i
+      end
+    end
+    median_household_income
   end
 end
--- test/economic_profile_repository_test.rb
+++ test/economic_profile_repository_test.rb
@@ -11,15 +11,68 @@ class TestEconomicProfileRepository < Minitest::Test
   
   def test_it_loads_data
     epr = EconomicProfileRepository.new
-    epr.load_data({
-                  :economic_profile => 
-                                      {
-                                      :median_household_income => "./data/Median household income.csv",
-                                      :children_in_poverty => "./data/School-aged children in poverty.csv",
-                                      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
-                                      :title_i => "./data/Title I students.csv"
-                                      }
-                  })
+    data = epr.load_data({
+                          :economic_profile => 
+                                              {
+                                              :median_household_income => "./data/Median household income.csv",
+                                              :children_in_poverty => "./data/School-aged children in poverty.csv",
+                                              :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                                              :title_i => "./data/Title I students.csv"
+                                              }
+                          })
+    assert_instance_of Hash, data
+  end
+
+  def test_find_by_name_can_return_enrollment_instance
+    epr = EconomicProfileRepository.new
+    data = epr.load_data({
+                          :economic_profile => 
+                                              {
+                                              :median_household_income => "./data/Median household income.csv",
+                                              :children_in_poverty => "./data/School-aged children in poverty.csv",
+                                              :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                                              :title_i => "./data/Title I students.csv"
+                                              }
+                          })
     ep = epr.find_by_name("ACADEMY 20")
+    assert_instance_of EconomicProfile, ep
+  end
+
+  def test_find_by_name_can_return_nil
+    epr = EconomicProfileRepository.new
+    data = epr.load_data({
+                          :economic_profile => 
+                                              {
+                                              :median_household_income => "./data/Median household income.csv",
+                                              :children_in_poverty => "./data/School-aged children in poverty.csv",
+                                              :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                                              :title_i => "./data/Title I students.csv"
+                                              }
+                          })
+    ep = epr.find_by_name("UNLIKELY NAME")
+    assert_nil ep
+  end
+    
+  def test_get_median_household_income
+    epr = EconomicProfileRepository.new
+    data_set = epr.load_data({
+                          :economic_profile => 
+                                              {
+                                              :median_household_income => "./data/Median household income.csv",
+                                              :children_in_poverty => "./data/School-aged children in poverty.csv",
+                                              :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                                              :title_i => "./data/Title I students.csv"
+                                              }
+                          })
+    district_name = "ACADEMY 20"
+    mhi = epr.get_median_household_income(data_set, district_name)
+    expected = {
+                [2005, 2009] => 85060, 
+                [2006, 2010] => 85450, 
+                [2008, 2012] => 89615, 
+                [2007, 2011] => 88099, 
+                [2009, 2013] => 89953
+              }
+    assert_equal expected, mhi
   end
 end
