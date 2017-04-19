4fc064400e998e9d33b1b1c3346729d5972a2a7a
ccorrigan <barnumswinter@gmail.com>
2017-04-18 19:20:09 -0600

    Start load_data functionality. Stop to merge with branch that contains EconomicProfile class

--- test/economic_profile_repository_test.rb
+++ test/economic_profile_repository_test.rb
@@ -9,4 +9,17 @@ class TestEconomicProfileRepository < Minitest::Test
     assert_instance_of EconomicProfileRepository, epr
   end
   
+  def test_it_loads_data
+    epr = EconomicProfileRepository.new
+    epr.load_data({
+                  :economic_profile => 
+                                      {
+                                      :median_household_income => "./data/Median household income.csv",
+                                      :children_in_poverty => "./data/School-aged children in poverty.csv",
+                                      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                                      :title_i => "./data/Title I students.csv"
+                                      }
+                  })
+    ep = epr.find_by_name("ACADEMY 20")
+  end
 end
