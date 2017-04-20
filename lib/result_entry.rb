2d1e7c9bb781b8e6648ca520f2a473cadfd5d55b
NataliaColome <nmcolome@gmail.com>
2017-04-20 00:44:04 -0600

    Fix result_set bug

--- lib/result_entry.rb
+++ lib/result_entry.rb
@@ -8,10 +8,5 @@ class ResultEntry
     @children_in_poverty_rate = result_data[:children_in_poverty_rate]
     @high_school_graduation_rate = result_data[:high_school_graduation_rate]
     @median_household_income = result_data[:median_household_income]
-
-    # @free_and_reduced_price_lunch_rate = result_data[:free_and_reduced_price_lunch_rate]
-    # @children_in_poverty_rate = result_data[:children_in_poverty_rate]
-    # @high_school_graduation_rate = result_data[:high_school_graduation_rate]
-    # @median_household_income = result_data[:median_household_income]
   end
 end
