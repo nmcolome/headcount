c45a3a34af4fb1778e0364046660248782461623
ccorrigan <barnumswinter@gmail.com>
2017-04-19 18:49:56 -0600

    district_repository code cleaned of white spaces, etc

--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -36,9 +36,20 @@ class TestDistrictRepository < Minitest::Test
     })
 
     assert_equal Hash, data.class
+    assert_instance_of CSV, data[:enrollment][:kindergarten]
+    assert_instance_of CSV, data[:enrollment][:high_school_graduation]
+    assert_instance_of CSV, data[:statewide_testing][:third_grade]
+    assert_instance_of CSV, data[:statewide_testing][:eighth_grade]
+    assert_instance_of CSV, data[:statewide_testing][:math]
+    assert_instance_of CSV, data[:statewide_testing][:reading]
+    assert_instance_of CSV, data[:statewide_testing][:writing] 
+    assert_instance_of CSV, data[:economic_profile][:median_household_income]
+    assert_instance_of CSV, data[:economic_profile][:children_in_poverty]
+    assert_instance_of CSV, data[:economic_profile][:free_or_reduced_price_lunch]
+    assert_instance_of CSV, data[:economic_profile][:title_i]
   end
 
-  def test_access_to_districts
+  def test_access_to_attributes
     dr = DistrictRepository.new
     data = dr.load_data({
       :enrollment => {
@@ -104,6 +115,12 @@ class TestDistrictRepository < Minitest::Test
         :math => "./test/fixtures/math_5lines.csv",
         :reading => "./test/fixtures/reading_5lines.csv",
         :writing => "./test/fixtures/writing_5lines.csv"
+      },
+      :economic_profile => {
+        :median_household_income => "./test/fixtures/academy_median.csv",
+        :children_in_poverty => "./test/fixtures/academy_children.csv",
+        :free_or_reduced_price_lunch => "./test/fixtures/academy_lunch.csv",
+        :title_i => "./test/fixtures/academy_title.csv"
       }
     })
 
@@ -125,6 +142,12 @@ class TestDistrictRepository < Minitest::Test
         :math => "./test/fixtures/math_5lines.csv",
         :reading => "./test/fixtures/reading_5lines.csv",
         :writing => "./test/fixtures/writing_5lines.csv"
+      },
+      :economic_profile => {
+        :median_household_income => "./test/fixtures/academy_median.csv",
+        :children_in_poverty => "./test/fixtures/academy_children.csv",
+        :free_or_reduced_price_lunch => "./test/fixtures/academy_lunch.csv",
+        :title_i => "./test/fixtures/academy_title.csv"
       }
     })
 
@@ -145,6 +168,12 @@ class TestDistrictRepository < Minitest::Test
         :math => "./test/fixtures/math_5lines.csv",
         :reading => "./test/fixtures/reading_5lines.csv",
         :writing => "./test/fixtures/writing_5lines.csv"
+      },
+      :economic_profile => {
+        :median_household_income => "./test/fixtures/academy_median.csv",
+        :children_in_poverty => "./test/fixtures/academy_children.csv",
+        :free_or_reduced_price_lunch => "./test/fixtures/academy_lunch.csv",
+        :title_i => "./test/fixtures/academy_title.csv"
       }
     })
 
@@ -317,4 +346,37 @@ class TestDistrictRepository < Minitest::Test
     assert_equal 0.857, output
   end
 
+  def test_districts_have_access_to_all_economic_profile_methods
+    dr = DistrictRepository.new
+    data = dr.load_data({
+      :enrollment => {
+        :kindergarten => "./test/fixtures/k_5lines.csv",
+        :high_school_graduation => "./test/fixtures/hs_5lines.csv",
+      },
+      :statewide_testing => {
+        :third_grade => "./test/fixtures/third_5lines.csv",
+        :eighth_grade => "./test/fixtures/eighth_5lines.csv",
+        :math => "./test/fixtures/math_5lines.csv",
+        :reading => "./test/fixtures/reading_5lines.csv",
+        :writing => "./test/fixtures/writing_5lines.csv"
+      },
+      :economic_profile => {
+        :median_household_income => "./test/fixtures/academy_median.csv",
+        :children_in_poverty => "./test/fixtures/academy_children.csv",
+        :free_or_reduced_price_lunch => "./test/fixtures/academy_lunch.csv",
+        :title_i => "./test/fixtures/academy_title.csv"
+      }
+    })
+
+    district = dr.find_by_name("ACADEMY 20")
+    assert_equal Hash, district.economic_profile.median_household_income.class
+    assert_equal Hash, district.economic_profile.children_in_poverty.class
+    assert_equal Hash, district.economic_profile.free_or_reduced_price_lunch.class
+    assert_equal Hash, district.economic_profile.title_i.class
+    refute district.economic_profile.median_household_income.empty?
+    refute district.economic_profile.children_in_poverty.empty?
+    refute district.economic_profile.free_or_reduced_price_lunch.empty?
+    refute district.economic_profile.title_i.empty?
+
+  end
 end
